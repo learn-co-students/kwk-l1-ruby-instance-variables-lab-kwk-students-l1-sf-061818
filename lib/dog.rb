8bc5b25ae5031db68a82d87e1ca868a1f2a85072
ismillet <isabella@duckler.com>
2018-06-21 23:20:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+class Dog 
+  
+  def name=(dogs_name) #setter 
+    @this_dogs_name = dogs_name
+    end 
+  
+  def name #getter 
+    @this_dogs_name 
+    end 
+end 
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+puts lassie.name 
